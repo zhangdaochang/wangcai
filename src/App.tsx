c3dde828d7690347a187dd22f1f4a90a9acd27a5
zhangdaochang <2051525775@qq.com>
2022-06-22 13:05:26 +0800

    remove unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import './App.scss';
-import "aaa/xxx.tsx"
-import Button from "components/Button";
-import Box from "./components/Box";
-
 
 function App() {
     return (
         <div className="App">
-            <Box>
-                <Button>你好</Button>
-            </Box>
         </div>
     );
 }
