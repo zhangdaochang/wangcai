ccf7f79dd10023d9c9c254941c8f828a0e4d4e5f
zhangdaochang <2051525775@qq.com>
2022-06-22 13:02:14 +0800

    remove unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import "aaa/xxx.tsx"
+import Button from "components/Button";
+import Box from "./components/Box";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box>
+                <Button>你好</Button>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
